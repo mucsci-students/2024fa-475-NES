1b0cee68105802193be1066c8bd609fb2f1699fe
derrickboyer3 <derrickboyer3455@gmail.com>
2024-10-05 11:00:30 -0400

    Added health bar scripts to all enemy prefabs

---
+++ Assets/Prefabs/Enemy/Ground Enemy/TallEnemyHealth.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public GameObject GameObject;
+    private EnemyStat enemyStat;
+    private float health;
+    private UnityEngine.Vector3 screenPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyStat = GameObject.GetComponent<EnemyStat>();
+        health = enemyStat.GetEnemyHealth();
+        screenPosition = Camera.main.WorldToScreenPoint(GameObject.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = enemyStat.GetEnemyHealth();
+        screenPosition = Camera.main.WorldToScreenPoint(GameObject.transform.position);
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(screenPosition.x - 50, Screen.height - screenPosition.y - 75, 100, 20), "Health: " + health);
+    }
+}
